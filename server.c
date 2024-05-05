29205ebac4e5691fd1c61488ff7464b436e61683
Marcel Wendler <marcel.wendler008@gmail.com>
2024-05-05 19:22:40 +0200

    fixed sum mistakes; added print statement to follow the server

--- server.c
+++ server.c
@@ -8,7 +8,8 @@
 void handle_request(int client_fd){
     char request[MAX_REQUEST_SIZE];
 
-    ssize_t request_size = recv(client_fd, request, MAX_REQUEST_SIZE, NULL);
+    ssize_t request_size = recv(client_fd, request, MAX_REQUEST_SIZE, 0);
+    printf("%s\n", request);
 }
 
 void server_shutdown(int server_fd){
@@ -28,12 +29,13 @@ int main(int argc, char **argv){
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
     // bind address and port to socket
-    if(bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1){
+    if(bind(server_fd, (struct sockaddr*)&server_addr, (socklen_t)sizeof(server_addr)) == -1){
         fprintf(stderr, "Error binding address and port to socket!/n");
         server_shutdown(server_fd);
     }
 
     // listen for connections
+    printf("Listening for connections...\n");
     if(listen(server_fd, 1) == -1){
         fprintf(stderr, "Error listening for connections!\n");
         server_shutdown(server_fd);
@@ -42,14 +44,20 @@ int main(int argc, char **argv){
     // accept connections
     while(1){
         struct sockaddr_in client_addr;
-        int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, sizeof(client_addr));
+        socklen_t client_addr_len = sizeof(client_addr);
+        int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_addr_len);
+
         if(client_fd == -1){
             fprintf(stderr, "Error accepting connection!\n");
             server_shutdown(server_fd);
+            break;
         }
+        printf("Accepted connection!\n");
 
         // handle the client request
         handle_request(client_fd);
+
+        shutdown(client_fd, SHUT_RDWR);
     }
 
     return 0;
