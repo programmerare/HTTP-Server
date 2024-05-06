9ffb2928808e1fe4edee905725f1fc5cf3eb0a81
Marcel Wendler <marcel.wendler008@gmail.com>
2024-05-06 10:12:52 +0200

    added comments above function

--- server.c
+++ server.c
@@ -11,14 +11,26 @@
 
 int server_fd;
 
-void parse_request(char *request){
+/*
+    parse_request
+    ---------------------
+    Description:
+    The parse_request function parses an http-request and extracts it's url using regex
+
+    Parameter:
+    char *request: A pointer to a string containing an http-request
+    char *url: A pointer to a string where the extracted url will be stored
+
+    Return:
+    void: The function does not return a value
+*/
+void parse_request(char *request, char *url){
     regex_t regex;
     regcomp(&regex, "^GET /([^ ]*) HTTP/1", REG_EXTENDED);
 
     size_t nmatch = 2;
     regmatch_t pmatch[2];
 
-    char url[MAX_URL_SIZE];
 
     if(regexec(&regex, request, nmatch, pmatch, 0) == 0){
         printf("Determined GET request!\n");
@@ -35,14 +47,42 @@ void parse_request(char *request){
     regfree(&regex);
 }
 
+/*
+    handle_request
+    ---------------------
+    Description:
+    This function recieves an http request using the recv function from the sys/socket library and reads it into a buffer.
+    It then calls the parse_request function on the request and retrieves the extracted url.
+
+    Parameter:
+    int client_fd: An integer working as a file descriptor. It is an identifier for the client's socket
+
+    Return:
+    void: This function does not return a value
+*/
 void handle_request(int client_fd){
     char request[MAX_REQUEST_SIZE];
+    char url[MAX_URL_SIZE];
 
     ssize_t request_size = recv(client_fd, request, MAX_REQUEST_SIZE, 0);
     printf("%s\n", request);
-    parse_request(request);
+    parse_request(request, url);
 }
 
+/*
+    server_shutdown
+    ---------------------
+    Description:
+    This function shuts down the server's socket by calling the shutdown function
+    of the sys/socket library on the server file descriptor.
+    It than exits the programin using exit(0);
+
+    Parameter:
+    No parametesr. The function is called on two signals SIGTERM and SIGINT defined in the signal library
+
+    Return:
+    void: This function does not return a value
+*/
 void server_shutdown(){
     printf("\nShutting down server...\n");
     shutdown(server_fd, SHUT_RDWR);
@@ -93,6 +133,7 @@ int main(int argc, char **argv){
         // handle the client request
         handle_request(client_fd);
 
+        printf("Shutting down client socket!\n");
         shutdown(client_fd, SHUT_RDWR);
     }
 
